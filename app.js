0ca1121fce8d34301b96f8115574c7efcf3689cf
Jordanzinho <joaop25042005@gmail.com>
2022-08-10 18:11:53 -0300

    Navagation Bar

--- app.js
+++ app.js
@@ -4,23 +4,23 @@ const path = require('path');
 
 app.set('view engine', 'ejs');
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.listen(3030)
 app.use(express.static(path.join(__dirname, 'public')));
-const playerController = require ('./controllers/player-controller');
+const playerController = require('./controllers/player-controller');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get ('/', function(req, res, next) {
-    res.render( 'index', {
+app.get('/', function (req, res, next) {
+    res.render('index', {
         title: "Famous Soccer Players",
         version: "0.0.0"
     });
 });
 
-app.get ('/futfamous',(req, res, next) => {
+app.get('/futfamous', (req, res, next) => {
     const playersData = playerController.getData();
-    res.render('futfamous', {playersData: playersData});
+    res.render('futfamous', { playersData: playersData });
 });
